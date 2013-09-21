e592daf3d690ee1fb76f369a65f782b366d2e91b
François Pelletier <franc00018@gmail.com>
2013-09-21 00:12:39 -0400

    ajout enonce exercice options

--- auto/serie1-exercices.el
+++ auto/serie1-exercices.el
@@ -2,5 +2,32 @@
  (lambda ()
     (LaTeX-add-labels
      "sec:serie-dexercices-1"
-     "sec:exercice-1-1")))
+     "sec:exercice-1-1")
+    (TeX-run-style-hooks
+     "tabularx"
+     "ccicons"
+     "scale=2"
+     "auto-pst-pdf"
+     "off"
+     "caption"
+     "hypcap"
+     "cleveref"
+     "hyperref"
+     "thmtools"
+     "amsthm"
+     "amsfonts"
+     "amsmath"
+     "inputenc"
+     "utf8"
+     "multirow"
+     "tikz"
+     "natbib"
+     "scrtime"
+     ""
+     "latex2e"
+     "art11"
+     "article"
+     "francais"
+     "english"
+     "11pt")))
 
